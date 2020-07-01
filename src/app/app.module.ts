7876b8b0ca542895f99d8b2d2ec7f75d59ef51b3
Duraimurugan <Duraimurugan.X.Rajendran.-ND@disney.com>
2020-07-01 06:49:52 -0400

    toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToggleComponent } from './toggle/toggle.component';
+import { SwitchComponent } from './switch/switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToggleComponent,
+    SwitchComponent
   ],
   imports: [
     BrowserModule
