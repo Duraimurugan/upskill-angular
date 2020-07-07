bcbfb6c04e1220ecf2fe745840812f0b08796601
Duraimurugan <durai.catch.me@gmail.com>
2020-07-06 21:07:00 -0400

    '@input'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
+
 export class AppComponent {
 
   showMessage(value) {
-    console.log(value);
+    console.log(value); 
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ToggleComponent } from './toggle/toggle.component';
 import { SwitchComponent } from './switch/switch.component';
+import { BankAccountComponent } from './bank-account/bank-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToggleComponent,
-    SwitchComponent
+    SwitchComponent,
+    BankAccountComponent
   ],
   imports: [
     BrowserModule
